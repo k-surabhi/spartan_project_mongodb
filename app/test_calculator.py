9c7b8ace0359385efc2111a60e50ca6518b7c425
Surabhi Kumari <surabhi.kumari@city.ac.uk>
2022-04-06 23:59:10 +0100

    added variables

--- app/test_calculator.py
+++ app/test_calculator.py
@@ -1,9 +1,7 @@
 def test_add():
-    assert add(4,3) == 6
     assert add(4,2) == 6
     assert add(3,3) == 6
     assert add(1,5) == 6
-    assert add(4,4) == 6
 
 def add(number1, number2):
     return number1 + number2
